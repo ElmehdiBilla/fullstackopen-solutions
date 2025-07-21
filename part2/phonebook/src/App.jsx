4d5b9cd56f3c9d75e0c2ee870fc7705663e8f5fd
elmehdibilla <billamehdi13@gmail.com>
2025-07-21 20:41:36 +0100

    solution for exercise 2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,39 +1,47 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+    const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+    const [newName, setNewName] = useState("");
 
-  const handleChange = (e) => {
-    setNewName(e.target.value);
-  }
+    const handleChange = (e) => {
+        setNewName(e.target.value);
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setPersons([...persons,{name:newName}])
-  }
-
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          name: <input value={newName} onChange={handleChange} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <div>
-        {
-          persons.map(person => <p key={person.name} >{person.name}</p>)
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let isExist = false;
+        persons.forEach((person) => {
+            if (person.name.toLowerCase() === newName.toLowerCase()) {
+                isExist = true;
+            }
+        });
+        if (isExist) {
+            alert(`${newName} is already added to phonebook`);
+            return;
         }
-      </div>
-    </div>
-  )
-}
+        setPersons([...persons, { name: newName }]);
+    };
 
-export default App
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    name: <input value={newName} onChange={handleChange} />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            <div>
+                {persons.map((person) => (
+                    <p key={person.name}>{person.name}</p>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default App;
