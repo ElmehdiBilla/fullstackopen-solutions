991d6585160e1bddcfe5365d5579feacca55a7a6
elmehdibilla <billamehdi13@gmail.com>
2025-07-29 17:49:29 +0100

    solution for exercise 2.20

--- part2/countries/src/components/Country.jsx
+++ part2/countries/src/components/Country.jsx
@@ -1,15 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Country = ({data}) => {
+    const [weather,setWeather] = useState(null);
     const languages = Object.keys(data.languages);
+    const lat=data.capitalInfo.latlng[0];
+    const lng=data.capitalInfo.latlng[1];
+    const apiKey= import.meta.env.VITE_API_KEY
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${apiKey}&units=metric`
+
+    useEffect(() => {
+        axios.get(url)
+        .then(response => setWeather(response.data))
+    },[])
 
     return (
         <div>
             <h1>{data.name.common}</h1>
-
             <p>Capital {data.capital}</p>
             <p>Area {data.area}</p>
 
             <h2>Languages</h2>
-
             <ul>
                 {
                     languages.map(l => <li key={l}>{data.languages[l]}</li>)
@@ -17,6 +28,16 @@ const Country = ({data}) => {
             </ul>
 
             <img src={data.flags.png} alt="" />
+
+            {
+                weather && 
+                <div>
+                    <h2>Weather in {data.capital}</h2>
+                    <p>Temperature {weather.main.temp} Celsius</p>
+                    <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].icon} />
+                    <p>Wind {weather.wind.speed} m/s</p>
+                </div>
+            }
 
         </div>
     )
