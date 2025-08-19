284f5b10112a1cea5657a37940a3e2dd2e491223
elmehdibilla <billamehdi13@gmail.com>
2025-08-19 21:14:17 +0100

    solution for exercise 3.13

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,30 +1,11 @@
+require('dotenv').config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
+const Person = require('./models/person');
 
-let persons = [
-    {
-        id: "1",
-        name: "Arto Hellas",
-        number: "040-123456",
-    },
-    {
-        id: "2",
-        name: "Ada Lovelace",
-        number: "39-44-5323523",
-    },
-    {
-        id: "3",
-        name: "Dan Abramov",
-        number: "12-43-234345",
-    },
-    {
-        id: "4",
-        name: "Mary Poppendieck",
-        number: "39-23-6423122",
-    },
-];
+let persons = [];
 
 app.use(express.static('dist'))
 app.use(express.json())
@@ -41,7 +22,9 @@ app.use((req, res, next) => {
 
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+        return response.json(persons)
+    })
 })
 
 app.get('/api/info', (request, response) => {
@@ -103,7 +86,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
