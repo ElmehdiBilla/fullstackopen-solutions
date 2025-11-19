11a6276263d3c8cd626e4e2d23ec61ea9c9ca8e2
elmehdibilla <billamehdi13@gmail.com>
2025-11-20 00:25:44 +0100

    solution for exercise 4.5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -6,7 +6,12 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+    return [...blogs].sort((blogA, blogB) => blogB.likes - blogA.likes)[0];
+};
+
 module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog,
 };
