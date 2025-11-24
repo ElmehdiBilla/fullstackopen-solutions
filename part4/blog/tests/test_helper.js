273504f4e207d815d1df50b3b254fb72b8fefac4
elmehdibilla <billamehdi13@gmail.com>
2025-11-24 21:27:37 +0100

    solution for exercise 4.8

---
+++ part4/blog/tests/test_helper.js
@@ -0,0 +1,20 @@
+const Blog = require("../models/blog");
+
+const initialBlogs = [
+    {
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7
+    },
+    {
+        title: "React patterns",
+        author: "Michael Chan",
+        url: "https://reactpatterns.com/",
+        likes: 7
+    }
+];
+
+module.exports = {
+  initialBlogs
+}
