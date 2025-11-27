8b0e9eb63597a24fd9be4c073616b43cbf2e6d80
elmehdibilla <billamehdi13@gmail.com>
2025-11-27 20:26:05 +0100

    solution for exercise 4.20

--- part4/blog/utils/middleware.js
+++ part4/blog/utils/middleware.js
@@ -1,3 +1,11 @@
+const tokenExtractor = (request, response, next) => {
+    const authorization = request.get("authorization");
+    if (authorization && authorization.startsWith("Bearer ")) {
+        request.token = authorization.replace("Bearer ", "");
+    }
+    next();
+};
+
 const errorHandler = (error, request, response, next) => {
     if (
         error.name === "MongoServerError" &&
@@ -19,4 +27,5 @@ const errorHandler = (error, request, response, next) => {
 
 module.exports = {
     errorHandler,
+    tokenExtractor,
 };
