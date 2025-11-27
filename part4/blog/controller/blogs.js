23b20c9c9221209492239ffb7990452efa752624
elmehdibilla <billamehdi13@gmail.com>
2025-11-27 19:04:53 +0100

    solution for exercise 4.17

--- part4/blog/controller/blogs.js
+++ part4/blog/controller/blogs.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogsRouter.get("/", async (request, response) => {
-    const blogs = await Blog.find({});
+    const blogs = await Blog.find({}).populate("user",{username: 1, name: 1});
     response.json(blogs);
 });
 
@@ -14,9 +15,15 @@ blogsRouter.post("/", async (request, response) => {
     if (!request.body.url) {
         return response.status(400).json({ error: "url is missing" });
     }
+    const user = await User.findOne({});
     const blog = new Blog(request.body);
-    const result = await blog.save();
-    response.status(201).json(result);
+    blog.user= user._id
+    
+    const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog._id) 
+    await user.save();
+
+    response.status(201).json(savedBlog);
 });
 
 blogsRouter.delete("/:id", async (request, response) => {
