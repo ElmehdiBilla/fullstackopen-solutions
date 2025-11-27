90646e4446151e732732e542f5374f3690ac387d
elmehdibilla <billamehdi13@gmail.com>
2025-11-27 20:08:08 +0100

    solution for exercise 4.19

--- part4/blog/controller/blogs.js
+++ part4/blog/controller/blogs.js
@@ -2,25 +2,50 @@ const mongoose = require("mongoose");
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 blogsRouter.get("/", async (request, response) => {
-    const blogs = await Blog.find({}).populate("user",{username: 1, name: 1});
+    const blogs = await Blog.find({}).populate("user", {
+        username: 1,
+        name: 1,
+    });
     response.json(blogs);
 });
 
+const getTokenFrom = (request) => {
+    const authorization = request.get("authorization");
+    if (authorization && authorization.startsWith("Bearer ")) {
+        return authorization.replace("Bearer ", "");
+    }
+    return null;
+};
+
 blogsRouter.post("/", async (request, response) => {
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: "token invalid" });
+    }
+
+    const user = await User.findById(decodedToken.id);
+
+    if (!user) {
+        return response
+            .status(400)
+            .json({ error: "UserId missing or not valid" });
+    }
     if (!request.body.title) {
         return response.status(400).json({ error: "title is missing" });
     }
     if (!request.body.url) {
         return response.status(400).json({ error: "url is missing" });
     }
-    const user = await User.findOne({});
+
     const blog = new Blog(request.body);
-    blog.user= user._id
-    
+    blog.user = user._id;
+
     const savedBlog = await blog.save();
-    user.blogs = user.blogs.concat(savedBlog._id) 
+    user.blogs = user.blogs.concat(savedBlog._id);
     await user.save();
 
     response.status(201).json(savedBlog);
