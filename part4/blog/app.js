455f35205e35cf02fba2dce349a676d2739fcb1c
elmehdibilla <billamehdi13@gmail.com>
2025-11-27 19:59:39 +0100

    solution for exercise 4.18

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -5,6 +5,7 @@ const logger = require("./utils/logger");
 const blogsRouter = require("./controller/blogs");
 const usersRouter = require("./controller/users");
 const middleware = require("./utils/middleware");
+const loginRouter = require('./controller/login')
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(express.json());
 
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 app.use(middleware.errorHandler);
 
