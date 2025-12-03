8fe385bb3f676aa4723486ba7d46cf2fbef38ef0
elmehdibilla <billamehdi13@gmail.com>
2025-12-03 17:04:19 +0100

    solution for exercise 5.18

--- part5/bloglist-E2E-testing/tests/blog_app.spec.js
+++ part5/bloglist-E2E-testing/tests/blog_app.spec.js
@@ -1,12 +1,43 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test');
+const { loginWith } = require('./helper');
 
 describe('Blog app', () => {
-    beforeEach(async ({ page }) => {
-        await page.goto('http://localhost:5173');
+    beforeEach(async ({ page, request }) => {
+        await request.post('/api/testing/reset');
+        await request.post('/api/users', {
+            data: {
+                name: 'Jhon Doe',
+                username: 'jhondoe',
+                password: 'password',
+            },
+        });
+
+        await page.goto('/');
     });
 
     test('Login form is shown', async ({ page }) => {
         const loginForm = page.getByText('log in to application').locator('..');
         await expect(loginForm).toBeVisible();
+    });
+
+    describe('Login', () => {
+        test('succeeds with correct credentials', async ({ page }) => {
+            await loginWith(page, 'jhondoe', 'password');
+            await expect(page.getByText('Jhon Doe logged in')).toBeVisible();
+        });
+
+        test('fails with wrong credentials', async ({ page }) => {
+            await loginWith(page, 'jhondoe', 'wrong');
+
+            const errorDiv = page.locator('.notification.error');
+            await expect(errorDiv).toContainText(
+                'invalid username or password'
+            );
+            await expect(errorDiv).toHaveCSS('border-style', 'solid');
+            await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)');
+            await expect(
+                page.getByText('Matti Luukkainen logged in')
+            ).not.toBeVisible();
+        });
     });
 });
