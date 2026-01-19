2d005fe95c4b12d73bc94bd0d3139f085cc30dfa
elmehdibilla <billamehdi13@gmail.com>
2026-01-19 18:58:09 +0100

    solution for exercise 8.14

--- part8/library/resolvers.js
+++ part8/library/resolvers.js
@@ -6,14 +6,23 @@ const resolvers = {
         bookCount: async () => Book.collection.countDocuments(),
         authorCount: async () => Author.collection.countDocuments(),
         allBooks: async (root, args) => {
-            // filters missing
-            return Book.find({}).populate('author');
+            const filter = {};
+            if (args.author) {
+                const author = await Author.findOne({ name: args.author });
+                if (author) filter.author = author._id;
+            }
+            if (args.genres && args.genres.length > 0) {
+                filter.genres = { $all: args.genres };
+            }
+
+            const books = await Book.find(filter).populate('author');
+            return books;
         },
         allAuthors: async () => Author.find({}),
     },
     Author: {
-        bookCount: (root) => {
-            return 0;
+        bookCount: async (root) => {
+            return await Book.find({ author: root.id }).countDocuments();
         },
     },
     Mutation: {
@@ -22,19 +31,19 @@ const resolvers = {
             if (!author) {
                 author = new Author({ born: null, name: args.author });
                 await author.save();
-            }            
+            }
             const book = new Book({ ...args, author: author._id });
-            await  book.save();
+            await book.save();
             return book.populate('author');
         },
-        editAuthor: (root, args) => {
-            let author = authors.find((a) => a.name === args.name);
+        editAuthor: async (root, args) => {
+            const author = await Author.findOne({ name: args.name });
 
             if (!author) {
                 return null;
             }
-            author = { ...author, born: args.setBornTo };
-            authors = authors.map((a) => (a.name === author.name ? author : a));
+            author.born = args.setBornTo;
+            await author.save();
             return author;
         },
     },
