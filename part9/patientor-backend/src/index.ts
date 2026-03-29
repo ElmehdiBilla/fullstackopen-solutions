a4b639c37d792b94b7c9ebc147868fb964272633
elmehdibilla <billamehdi13@gmail.com>
2026-03-29 15:19:15 +0100

    solution for exercise 9.10

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import diagnosesRouter from './routes/diagnoses';
 
 const app = express();
 
@@ -11,6 +12,8 @@ const PORT = 3001;
 app.get('/api/ping', (_req, res) => {
     res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
