4928e914b816b6bc05774025e8231fcf477709a4
elmehdibilla <billamehdi13@gmail.com>
2026-04-01 15:35:36 +0100

    solution for exercise 9.14

--- part9/patientor-backend/src/routes/patients.ts
+++ part9/patientor-backend/src/routes/patients.ts
@@ -1,7 +1,8 @@
-import express, { Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import patientsService from '../services/patientsService';
-import { NonSensitivePatient } from '../types';
-import toNewPatientEntry from '../utils';
+import { NewPatientEntry, NonSensitivePatient, Patient } from '../types';
+import { NewPatientSchema } from '../utils';
+import { z } from 'zod';
 
 const router = express.Router();
 
@@ -9,18 +10,28 @@ router.get('/', (_req, res: Response<NonSensitivePatient[]>) => {
     res.send(patientsService.getNonSensitiveEntries());
 });
 
-router.post('/', (req, res) => {
-    try{
-        const newPatientEntry = toNewPatientEntry(req.body);
-        const addedEntry = patientsService.addPatient(newPatientEntry);
-        res.json(addedEntry);
-    }catch(error:unknown){
-        let errorMessage = 'Something went wrong.';
-        if (error instanceof Error) {
-            errorMessage += ' Error: ' + error.message;
-        }
-        res.status(400).send(errorMessage);
+const newPatientParser = (req: Request, _res: Response, next: NextFunction) => {
+    try {
+        NewPatientSchema.parse(req.body);
+        next();
+    } catch (error: unknown) {
+        next(error);
     }
+};
+
+const errorMiddleware = (error: unknown, _req: Request, res: Response, next: NextFunction) => {
+    if (error instanceof z.ZodError) {
+        res.status(400).send({ error: error.issues });
+    } else {
+        next(error);
+    }
+};
+
+router.post('/', newPatientParser, (req: Request<unknown, unknown, NewPatientEntry>, res: Response<Patient>) => {
+    const addedEntry = patientsService.addPatient(req.body);
+    res.json(addedEntry);
 });
+
+router.use(errorMiddleware);
 
 export default router;
--- part9/patientor-backend/src/types.ts
+++ part9/patientor-backend/src/types.ts
@@ -1,3 +1,6 @@
+import { z } from 'zod';
+import { NewPatientSchema } from './utils';
+
 export interface Diagnosis {
     code: string;
     name: string;
@@ -20,4 +23,4 @@ export interface Patient {
 }
 
 export type NonSensitivePatient = Omit<Patient, 'ssn'>;
-export type NewPatientEntry = Omit<Patient, 'id'>;
+export type NewPatientEntry = z.infer<typeof NewPatientSchema>;
