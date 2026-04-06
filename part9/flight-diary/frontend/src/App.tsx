a76dfbe127b4bc36f6616fc57de33bbae2612cc9
elmehdibilla <billamehdi13@gmail.com>
2026-04-06 18:35:34 +0100

    solution for exercise 9.18

--- part9/flight-diary/frontend/src/App.tsx
+++ part9/flight-diary/frontend/src/App.tsx
@@ -1,5 +1,5 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 interface Diary {
     id: number;
@@ -10,26 +10,72 @@ interface Diary {
 }
 
 function App() {
-  const [diaries, setDiaries] = useState<Diary[]>([]);
+    const [diaries, setDiaries] = useState<Diary[]>([]);
+    const [date, setDate] = useState('');
+    const [visibility, setVisibility] = useState('');
+    const [weather, setWeather] = useState('');
+    const [comment, setComment] = useState('');
 
-  useEffect(() => {
-      axios.get<Diary[]>('http://localhost:3000/api/diaries').then((response) => {
-          setDiaries(response.data);
-      });
-  }, []);
+    useEffect(() => {
+        axios.get<Diary[]>('http://localhost:3000/api/diaries').then((response) => {
+            setDiaries(response.data);
+        });
+    }, []);
 
-  return (
-      <div>
-          <h2>Diary entries</h2>
-              {diaries.map((diary) => (
-                  <div key={diary.id}>
-                      <h3>{diary.date}</h3>
-                      <p>visibility: {diary.visibility}</p>
-                      <p>weather: {diary.weather}</p>
-                  </div>
-              ))}
-      </div>
-  );
+    const diaryCreation = (event: React.SyntheticEvent) => {
+        event.preventDefault();
+        const diaryToAdd = {
+            date,
+            visibility,
+            weather,
+            comment,
+        };
+        axios.post<Diary>('http://localhost:3000/api/diaries', diaryToAdd).then((response) => {
+            setDiaries(diaries.concat(response.data));
+        });
+
+        setDate('');
+        setVisibility('');
+        setWeather('');
+        setComment('');
+    };
+
+    return (
+        <div>
+            <h2>Add new entry</h2>
+            <form onSubmit={diaryCreation}>
+                <p>
+                    <label htmlFor='date'>date</label>
+                    <input name='date' value={date} onChange={(event) => setDate(event.target.value)} />
+                </p>
+                <p>
+                    <label htmlFor='visibility'>visibility</label>
+                    <input
+                        name='visibility'
+                        value={visibility}
+                        onChange={(event) => setVisibility(event.target.value)}
+                    />
+                </p>
+                <p>
+                    <label htmlFor='weather'>weather</label>
+                    <input name='weather' value={weather} onChange={(event) => setWeather(event.target.value)} />
+                </p>
+                <p>
+                    <label htmlFor='comment'>comment</label>
+                    <input name='comment' value={comment} onChange={(event) => setComment(event.target.value)} />
+                </p>
+                <button type='submit'>add</button>
+            </form>
+            <h2>Diary entries</h2>
+            {diaries.map((diary) => (
+                <div key={diary.id}>
+                    <h3>{diary.date}</h3>
+                    <p>visibility: {diary.visibility}</p>
+                    <p>weather: {diary.weather}</p>
+                </div>
+            ))}
+        </div>
+    );
 }
 
-export default App
+export default App;
