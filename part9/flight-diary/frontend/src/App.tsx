e2a062913630f6fb574c71cd29c56f96ac9d45af
elmehdibilla <billamehdi13@gmail.com>
2026-04-07 19:11:41 +0100

    solution for exercise 9.20

--- part9/flight-diary/frontend/src/App.tsx
+++ part9/flight-diary/frontend/src/App.tsx
@@ -12,8 +12,8 @@ interface Diary {
 function App() {
     const [diaries, setDiaries] = useState<Diary[]>([]);
     const [date, setDate] = useState('');
-    const [visibility, setVisibility] = useState('');
-    const [weather, setWeather] = useState('');
+    const [visibility, setVisibility] = useState<Diary['visibility']>('great');
+    const [weather, setWeather] = useState<Diary['weather']>('sunny');
     const [comment, setComment] = useState('');
     const [error, setError] = useState('');
 
@@ -43,8 +43,8 @@ function App() {
         .then((response) => {
             setDiaries(diaries.concat(response.data));
             setDate('');
-            setVisibility('');
-            setWeather('');
+            setVisibility('great');
+            setWeather('sunny');
             setComment('');
         }).catch(error => {
             if (axios.isAxiosError(error)) {
@@ -58,23 +58,117 @@ function App() {
     return (
         <div>
             <h2>Add new entry</h2>
-            <p style={{color:"red"}} >{error}</p>
+            <p style={{ color: 'red' }}>{error}</p>
             <form onSubmit={diaryCreation}>
                 <p>
                     <label htmlFor='date'>date</label>
-                    <input name='date' value={date} onChange={(event) => setDate(event.target.value)} />
+                    <input type='date' name='date' value={date} onChange={(event) => setDate(event.target.value)} />
                 </p>
                 <p>
-                    <label htmlFor='visibility'>visibility</label>
-                    <input
-                        name='visibility'
-                        value={visibility}
-                        onChange={(event) => setVisibility(event.target.value)}
-                    />
+                    <label htmlFor='visibility'>visibility </label>
+
+                    <label>
+                        great
+                        <input
+                            type='radio'
+                            name='visibility'
+                            value='great'
+                            checked={visibility === 'great'}
+                            onChange={() => setVisibility('great')}
+                        />
+                    </label>
+
+                    <label>
+                        good
+                        <input
+                            type='radio'
+                            name='visibility'
+                            value='good'
+                            checked={visibility === 'good'}
+                            onChange={() => setVisibility('good')}
+                        />
+                    </label>
+
+                    <label>
+                        ok
+                        <input
+                            type='radio'
+                            name='visibility'
+                            value='ok'
+                            checked={visibility === 'ok'}
+                            onChange={() => setVisibility('ok')}
+                        />
+                    </label>
+
+                    <label>
+                        poor
+                        <input
+                            type='radio'
+                            name='visibility'
+                            value='poor'
+                            checked={visibility === 'poor'}
+                            onChange={() => setVisibility('poor')}
+                        />
+                    </label>
                 </p>
+
                 <p>
-                    <label htmlFor='weather'>weather</label>
-                    <input name='weather' value={weather} onChange={(event) => setWeather(event.target.value)} />
+                    <label htmlFor='weather'>weather </label>
+
+                    <label>
+                        sunny
+                        <input
+                            type='radio'
+                            name='weather'
+                            value='sunny'
+                            checked={weather === 'sunny'}
+                            onChange={() => setWeather('sunny')}
+                        />
+                    </label>
+
+                    <label>
+                        rainy
+                        <input
+                            type='radio'
+                            name='weather'
+                            value='rainy'
+                            checked={weather === 'rainy'}
+                            onChange={() => setWeather('rainy')}
+                        />
+                    </label>
+
+                    <label>
+                        cloudy
+                        <input
+                            type='radio'
+                            name='weather'
+                            value='cloudy'
+                            checked={weather === 'cloudy'}
+                            onChange={() => setWeather('cloudy')}
+                        />
+                    </label>
+
+                    <label>
+                        stormy
+                        <input
+                            type='radio'
+                            name='weather'
+                            value='stormy'
+                            checked={weather === 'stormy'}
+                            onChange={() => setWeather('stormy')}
+                        />
+                    </label>
+
+                    <label>
+                        windy
+                        <input
+                            type='radio'
+                            name='weather'
+                            value='windy'
+                            checked={weather === 'windy'}
+                            onChange={() => setWeather('windy')}
+                        />
+                    </label>
                 </p>
                 <p>
                     <label htmlFor='comment'>comment</label>
