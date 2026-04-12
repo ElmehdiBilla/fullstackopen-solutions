3c3dcfc15fd59cf0b319457602fc9fd5fae17522
elmehdibilla <billamehdi13@gmail.com>
2026-04-12 15:55:58 +0100

    solution for exercise 9.22

--- part9/patientor-frontend/src/components/PatientListPage/index.tsx
+++ part9/patientor-frontend/src/components/PatientListPage/index.tsx
@@ -8,6 +8,7 @@ import AddPatientModal from "../AddPatientModal";
 import HealthRatingBar from "../HealthRatingBar";
 
 import patientService from "../../services/patients";
+import { Link } from "react-router-dom";
 
 interface Props {
   patients : Patient[]
@@ -48,44 +49,41 @@ const PatientListPage = ({ patients, setPatients } : Props ) => {
   };
 
   return (
-    <div className="App">
-      <Box>
-        <Typography align="center" variant="h6">
-          Patient list
-        </Typography>
-      </Box>
-      <Table sx={{ marginBottom: "1em" }}>
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell>Gender</TableCell>
-            <TableCell>Occupation</TableCell>
-            <TableCell>Health Rating</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {Object.values(patients).map((patient: Patient) => (
-            <TableRow key={patient.id}>
-              <TableCell>{patient.name}</TableCell>
-              <TableCell>{patient.gender}</TableCell>
-              <TableCell>{patient.occupation}</TableCell>
-              <TableCell>
-                <HealthRatingBar showText={false} rating={1} />
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <AddPatientModal
-        modalOpen={modalOpen}
-        onSubmit={submitNewPatient}
-        error={error}
-        onClose={closeModal}
-      />
-      <Button variant="contained" onClick={() => openModal()}>
-        Add New Patient
-      </Button>
-    </div>
+      <div className='App'>
+          <Box>
+              <Typography align='center' variant='h6'>
+                  Patient list
+              </Typography>
+          </Box>
+          <Table sx={{ marginBottom: '1em' }}>
+              <TableHead>
+                  <TableRow>
+                      <TableCell>Name</TableCell>
+                      <TableCell>Gender</TableCell>
+                      <TableCell>Occupation</TableCell>
+                      <TableCell>Health Rating</TableCell>
+                  </TableRow>
+              </TableHead>
+              <TableBody>
+                  {Object.values(patients).map((patient: Patient) => (
+                      <TableRow key={patient.id}>
+                          <TableCell>
+                              <Link to={`/patients/${patient.id}`}>{patient.name}</Link>
+                          </TableCell>
+                          <TableCell>{patient.gender}</TableCell>
+                          <TableCell>{patient.occupation}</TableCell>
+                          <TableCell>
+                              <HealthRatingBar showText={false} rating={1} />
+                          </TableCell>
+                      </TableRow>
+                  ))}
+              </TableBody>
+          </Table>
+          <AddPatientModal modalOpen={modalOpen} onSubmit={submitNewPatient} error={error} onClose={closeModal} />
+          <Button variant='contained' onClick={() => openModal()}>
+              Add New Patient
+          </Button>
+      </div>
   );
 };
 
---
+++ part9/patientor-frontend/src/components/PatientPage/index.tsx
@@ -0,0 +1,48 @@
+import { Typography } from '@mui/material';
+import { Patient } from '../../types';
+import { Female, Male } from '@mui/icons-material';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import patientService from '../../services/patients';
+
+const PatientPage = () => {
+    const { id  } = useParams<{id: string}>();
+    const [patient, setPatient] = useState<Patient>();
+
+    useEffect(() => {
+        if (!id) return;
+
+        const fetchPatient = async () => {
+            try {
+                const data = await patientService.findById(id);
+                setPatient(data);
+            } catch (e: unknown) {
+                if (axios.isAxiosError(e)) {
+                    if (e?.response?.data && typeof e?.response?.data === 'string') {
+                        const message = e.response.data.replace('Something went wrong. Error: ', '');
+                        console.error(message);
+                    } else {
+                        console.error('Unrecognized axios error');
+                    }
+                } else {
+                    console.error('Unknown error', e);
+                }
+            }
+        };
+
+        void fetchPatient();
+    }, [id]);
+
+    return (
+        <div className='App'>
+            <Typography variant='h4' sx={{ marginBottom: '0.5em' }}>
+                {patient?.name} {patient?.gender === 'female' ? <Female fontSize='large' /> : <Male fontSize='large' />}
+            </Typography>
+            <Typography variant='h6'>ssn: {patient?.ssn}</Typography>
+            <Typography variant='h6'>occupation: {patient?.occupation}</Typography>
+        </div>
+    );
+};
+
+export default PatientPage;
