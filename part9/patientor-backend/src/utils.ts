54fbe5e45f6e61726e1d00b523cf6ba40b8dfc90
elmehdibilla <billamehdi13@gmail.com>
2026-04-13 16:33:26 +0100

    solution for exercise 9.23

--- part9/patientor-backend/src/utils.ts
+++ part9/patientor-backend/src/utils.ts
@@ -1,5 +1,5 @@
 import { z } from 'zod';
-import { Gender, NewPatientEntry } from './types';
+import { Entry, Gender, NewPatientEntry } from './types';
 
 export const NewPatientSchema = z.object({
     name: z.string(),
@@ -11,6 +11,25 @@ export const NewPatientSchema = z.object({
 
 const toNewPatientEntry = (object: unknown): NewPatientEntry => {
     return NewPatientSchema.parse(object);
+};
+
+const isEntryType = (type: unknown): type is Entry['type'] => {
+    return typeof type === 'string' && ['Hospital', 'OccupationalHealthcare', 'HealthCheck'].includes(type);
+};
+
+export const parseEntries = (entries: unknown[]): Entry[] => {
+    return entries.map((entry) => {
+        if (
+            !entry ||
+            typeof entry !== 'object' ||
+            !('type' in entry) ||
+            !isEntryType((entry as { type: unknown }).type)
+        ) {
+            throw new Error('Invalid entry');
+        }
+
+        return entry as Entry;
+    });
 };
 
 export default toNewPatientEntry;
