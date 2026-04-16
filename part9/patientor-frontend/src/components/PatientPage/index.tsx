3f4af56d16491ac54da1d422e687d72000352328
elmehdibilla <billamehdi13@gmail.com>
2026-04-16 15:02:03 +0100

    solution for exercise 9.26

--- part9/patientor-frontend/src/components/PatientPage/index.tsx
+++ part9/patientor-frontend/src/components/PatientPage/index.tsx
@@ -1,6 +1,6 @@
-import { Typography } from '@mui/material';
-import { Diagnosis, Patient } from '../../types';
-import { Female, Male } from '@mui/icons-material';
+import { Card, CardContent, Typography } from '@mui/material';
+import { Diagnosis, Patient, HospitalEntry, OccupationalHealthcareEntry, HealthCheckEntry, Entry } from '../../types';
+import { Favorite, Female, LocalHospital, Male, MedicalServices, Work } from '@mui/icons-material';
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
@@ -10,7 +10,74 @@ interface Props {
     diagnoses: Diagnosis[];
 }
 
-const PatientPage = ({ diagnoses }: Props) => {
+const HealthCheckColors = ['green', 'yellow', 'orange', 'red'];
+
+const assertNever = (value: never): never => {
+    throw new Error(`Unhandled case: ${value}`);
+};
+
+const EntryDetails = ({ entry }: { entry: Entry }) => {
+    switch (entry.type) {
+        case 'Hospital':
+            return <HospitalElement entry={entry}/>;
+
+        case 'OccupationalHealthcare':
+            return <OccupationalHealthcareElement entry={entry} />;
+
+        case 'HealthCheck':
+            return <HealthCheckElement entry={entry} />;
+
+        default:
+            return assertNever(entry);
+    }
+};
+
+const HospitalElement = ({ entry }: { entry: HospitalEntry  }) => {
+    return (
+        <Card key={entry.id} sx={{ marginBottom: '0.5em' }}>
+            <CardContent>
+                <Typography variant='h6'>
+                    {entry.date} <LocalHospital />
+                </Typography>
+                <Typography variant='h6'>{entry.description}</Typography>
+                <Typography variant='h6'>diagnose by {entry.specialist}</Typography>
+            </CardContent>
+        </Card>
+    );
+};
+
+const OccupationalHealthcareElement = ({ entry }: { entry: OccupationalHealthcareEntry }) => {
+    return (
+        <Card key={entry.id} sx={{ marginBottom: '0.5em' }}>
+            <CardContent>
+                <Typography variant='h6'>
+                    {entry.date} <Work /> {entry.employerName}
+                </Typography>
+                <Typography variant='h6'>{entry.description}</Typography>
+                <Typography variant='h6'>diagnose by {entry.specialist}</Typography>
+            </CardContent>
+        </Card>
+    );
+};
+
+const HealthCheckElement = ({ entry }: { entry: HealthCheckEntry }) => {
+    return (
+        <Card key={entry.id} sx={{ marginBottom: '0.5em' }}>
+            <CardContent>
+                <Typography variant='h6'>
+                    {entry.date} <MedicalServices />
+                </Typography>
+                <Typography variant='h6'>{entry.description}</Typography>
+                <Typography variant='h6'>
+                    <Favorite sx={{ color: HealthCheckColors[entry.healthCheckRating] }} />
+                </Typography>
+                <Typography variant='h6'>diagnose by {entry.specialist}</Typography>
+            </CardContent>
+        </Card>
+    );
+};
+
+const PatientPage = ({ diagnoses: _diagnoses }: Props) => {
     const { id } = useParams<{ id: string }>();
     const [patient, setPatient] = useState<Patient>();
 
@@ -48,22 +115,7 @@ const PatientPage = ({ diagnoses }: Props) => {
             <Typography variant='h5' sx={{ marginBottom: '0.5em', marginTop: '0.5em' }}>
                 entries
             </Typography>
-            {patient?.entries.map((entry) => {
-                return (
-                    <div>
-                        <Typography variant='h6'>
-                            {entry.date} {entry.description}
-                        </Typography>
-                        <ul>
-                            {entry.diagnosisCodes?.map((code) => (
-                                <li>
-                                    <Typography>{code} {diagnoses.find((diagnosis) => diagnosis.code === code)?.name}</Typography>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                );
-            })}
+            {patient?.entries.map((entry) => <EntryDetails entry={entry}/>)}
         </div>
     );
 };
