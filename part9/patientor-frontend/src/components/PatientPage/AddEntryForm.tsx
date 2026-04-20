9449ee6939877b0636c833f604e6be98c08b608f
elmehdibilla <billamehdi13@gmail.com>
2026-04-20 13:55:14 +0100

    solution for exercise 9.29

--- part9/patientor-frontend/src/components/PatientPage/AddEntryForm.tsx
+++ part9/patientor-frontend/src/components/PatientPage/AddEntryForm.tsx
@@ -1,5 +1,15 @@
 import { SyntheticEvent, useState } from 'react';
-import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+import {
+    Box,
+    Button,
+    FormControl,
+    FormControlLabel,
+    FormLabel,
+    Radio,
+    RadioGroup,
+    Stack,
+    TextField,
+} from '@mui/material';
 import { EntryFormValues } from '../../types';
 
 interface Props {
@@ -8,12 +18,119 @@ interface Props {
     onSubmit: (values: EntryFormValues) => void;
 }
 
+interface OccupationalHealthcareProps {
+    setEmployerName: React.Dispatch<React.SetStateAction<string>>;
+    setStartDate: React.Dispatch<React.SetStateAction<string>>;
+    setEndDate: React.Dispatch<React.SetStateAction<string>>;
+    employerName: string;
+    startDate: string;
+    endDate: string;
+}
+
+interface HospitalProps {
+    setDate: React.Dispatch<React.SetStateAction<string>>;
+    setCriteria: React.Dispatch<React.SetStateAction<string>>;
+    date: string;
+    criteria: string;
+}
+
+interface HealthCheckProps {
+    setHealthCheckRating: React.Dispatch<React.SetStateAction<string>>;
+    healthCheckRating: string;
+}
+
+type EntryType = 'Hospital' | 'OccupationalHealthcare' | 'HealthCheck';
+
+const OccupationalHealthcareElement = ({
+    employerName,
+    startDate,
+    endDate,
+    setEmployerName,
+    setStartDate,
+    setEndDate,
+}: OccupationalHealthcareProps) => {
+    return (
+        <Box>
+            <TextField
+                label='Employer Name'
+                fullWidth
+                variant='standard'
+                value={employerName}
+                onChange={({ target }) => setEmployerName(target.value)}
+            />
+            <FormControl fullWidth sx={{ pt: 3 }}>
+                <FormLabel>SickLeave</FormLabel>
+                <Stack sx={{ px: 2 }}>
+                    <TextField
+                        label='Start'
+                        fullWidth
+                        variant='standard'
+                        value={startDate}
+                        onChange={({ target }) => setStartDate(target.value)}
+                    />
+                    <TextField
+                        label='End'
+                        fullWidth
+                        variant='standard'
+                        value={endDate}
+                        onChange={({ target }) => setEndDate(target.value)}
+                    />
+                </Stack>
+            </FormControl>
+        </Box>
+    );
+};
+
+const HospitalElement = ({ date, criteria, setDate, setCriteria }: HospitalProps) => {
+    return (
+        <Box>
+            <FormControl fullWidth sx={{ pt: 2 }}>
+                <FormLabel>Discharge</FormLabel>
+
+                <Stack sx={{ px: 2 }}>
+                    <TextField
+                        label='Date'
+                        variant='standard'
+                        value={date}
+                        onChange={({ target }) => setDate(target.value)}
+                    />
+
+                    <TextField
+                        label='Criteria'
+                        variant='standard'
+                        value={criteria}
+                        onChange={({ target }) => setCriteria(target.value)}
+                    />
+                </Stack>
+            </FormControl>
+        </Box>
+    );
+};
+
+const HealthCheckElement = ({ healthCheckRating, setHealthCheckRating }: HealthCheckProps) => {
+    return (
+        <TextField
+            label='healthCheckRating'
+            fullWidth
+            variant='standard'
+            value={healthCheckRating}
+            onChange={({ target }) => setHealthCheckRating(target.value)}
+        />
+    );
+};
+
 const AddEntryForm = ({ visibility, onSubmit, onClose }: Props) => {
+    const [type, setType] = useState<EntryType>('HealthCheck');
     const [description, setDescription] = useState('');
     const [date, setDate] = useState('');
     const [specialist, setSpecialist] = useState('');
     const [healthCheckRating, setHealthCheckRating] = useState('');
     const [diagnosisCodes, setDiagnosisCodes] = useState('');
+    const [employerName, setEmployerName] = useState('');
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+    const [dischargeDate, setDischargeDate] = useState('');
+    const [criteria, setcriteria] = useState('');
 
     const closeForm = (): void => {
         setDescription('');
@@ -24,23 +141,75 @@ const AddEntryForm = ({ visibility, onSubmit, onClose }: Props) => {
         onClose();
     };
 
-    const addPatient = (event: SyntheticEvent) => {
-        event.preventDefault();
-        onSubmit({
-            type:'HealthCheck',
-            description,
-            date,
-            specialist,
-            healthCheckRating: Number(healthCheckRating),
-            diagnosisCodes: diagnosisCodes.split(','),
-        });
+const addPatient = (event: SyntheticEvent) => {
+    event.preventDefault();
+
+    const baseEntry = {
+        type,
+        description,
+        date,
+        specialist,
+        diagnosisCodes: diagnosisCodes.split(','),
     };
 
+    let entry: EntryFormValues;
+
+    switch (type) {
+        case 'Hospital':
+            entry = {
+                ...baseEntry,
+                type: 'Hospital',
+                discharge: {
+                    date: dischargeDate,
+                    criteria,
+                },
+            };
+            break;
+
+        case 'OccupationalHealthcare':
+            entry = {
+                ...baseEntry,
+                type: 'OccupationalHealthcare',
+                employerName,
+                sickLeave: {
+                    startDate,
+                    endDate,
+                },
+            };
+            break;
+
+        case 'HealthCheck':
+            entry = {
+                ...baseEntry,
+                type: 'HealthCheck',
+                healthCheckRating: Number(healthCheckRating),
+            };
+            break;
+
+        default:
+            throw new Error('Unknown entry type');
+    }
+
+    onSubmit(entry);
+};
+
     return (
-        <Box component='section' sx={{ p: 2, border: '1px dashed grey', display:visibility ? 'block' : 'none' }}>
+        <Box component='section' sx={{ p: 2, border: '1px dashed grey', display: visibility ? 'block' : 'none' }}>
             <form onSubmit={addPatient}>
                 <Stack spacing={2}>
-                    <Typography variant='h6'>New HealtCheck entry</Typography>
+                    <FormControl>
+                        <FormLabel>Entry Type</FormLabel>
+                        <RadioGroup row value={type} onChange={({ target }) => setType(target.value as EntryType)}>
+                            <FormControlLabel value='HealthCheck' control={<Radio />} label='HealthCheck' />
+                            <FormControlLabel value='Hospital' control={<Radio />} label='Hospital' />
+                            <FormControlLabel
+                                value='OccupationalHealthcare'
+                                control={<Radio />}
+                                label='OccupationalHealthcare'
+                            />
+                        </RadioGroup>
+                    </FormControl>
+
                     <TextField
                         label='Description'
                         fullWidth
@@ -63,19 +232,45 @@ const AddEntryForm = ({ visibility, onSubmit, onClose }: Props) => {
                         onChange={({ target }) => setSpecialist(target.value)}
                     />
                     <TextField
-                        label='healthCheckRating'
-                        fullWidth
-                        variant='standard'
-                        value={healthCheckRating}
-                        onChange={({ target }) => setHealthCheckRating(target.value)}
-                    />
-                    <TextField
                         label='diagnosis Codes'
                         fullWidth
                         variant='standard'
                         value={diagnosisCodes}
                         onChange={({ target }) => setDiagnosisCodes(target.value)}
                     />
+                    {(() => {
+                        switch (type) {
+                            case 'HealthCheck':
+                                return (
+                                    <HealthCheckElement
+                                        healthCheckRating={healthCheckRating}
+                                        setHealthCheckRating={setHealthCheckRating}
+                                    />
+                                );
+                            case 'OccupationalHealthcare':
+                                return (
+                                    <OccupationalHealthcareElement
+                                        employerName={employerName}
+                                        startDate={startDate}
+                                        endDate={endDate}
+                                        setEmployerName={setEmployerName}
+                                        setStartDate={setStartDate}
+                                        setEndDate={setEndDate}
+                                    />
+                                );
+                            case 'Hospital':
+                                return (
+                                    <HospitalElement
+                                        date={dischargeDate}
+                                        criteria={criteria}
+                                        setDate={setDischargeDate}
+                                        setCriteria={setcriteria}
+                                    />
+                                );
+                            default:
+                                break;
+                        }
+                    })()}
                     <Stack
                         spacing={2}
                         direction='row'
