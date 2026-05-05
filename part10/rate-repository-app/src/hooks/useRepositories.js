8d0cb45b26944076cc54df729357b89b9d5bc7a0
elmehdibilla <billamehdi13@gmail.com>
2026-05-05 18:21:49 +0100

    solution for exercise 10.11

---
+++ part10/rate-repository-app/src/hooks/useRepositories.js
@@ -0,0 +1,10 @@
+import { useQuery } from '@apollo/client/react';
+import { GET_REPOSITORIES } from '../graphql/queries';
+
+const useRepositories = () => {
+    const { data, loading } = useQuery(GET_REPOSITORIES, { fetchPolicy: 'cache-and-network' });
+
+    return { repositories: data?.repositories, loading };
+};
+
+export default useRepositories;
